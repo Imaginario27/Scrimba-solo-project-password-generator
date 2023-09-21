733f636e67969f6a6c532b50acf5c04c4b0745bd
Imaginario27 <imagchine27@gmail.com>
2023-09-21 13:01:28 +0200

    Improvement update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Character array
 let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+// Symbol array
 let charactersSymbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
+// Combined array
 let charactersWithSymbols = characters.concat(charactersSymbols);
 
+// Variables
 let passwordLength = document.getElementById("password-length").value
 let passwordField1 = document.getElementById("password-field-1")
 let passwordField2 = document.getElementById("password-field-2")
@@ -11,6 +15,7 @@ let infoField = document.getElementById("info")
 let symbols = document.getElementById("symbols")
 let hasSymbol = true
 
+// Gets the random index
 function getRandomIndex(){
     let randomIndex = 0
     
@@ -23,6 +28,7 @@ function getRandomIndex(){
     return randomIndex
 }
 
+// Generates random password
 function generatePasswords(){
     passwordField1.value = ""
     passwordField2.value = ""
@@ -32,13 +38,14 @@ function generatePasswords(){
         if (hasSymbol){
             passwordField1.value += charactersWithSymbols[getRandomIndex()]
             passwordField2.value += charactersWithSymbols[getRandomIndex()] 
-        }else{
+        } else{
             passwordField1.value += characters[getRandomIndex()]
             passwordField2.value += characters[getRandomIndex()] 
         }
     }
 }
 
+// Activate symbol filtering
 function activateSymbolFilter(){
     if(symbols.checked){
         hasSymbol = true
@@ -47,10 +54,12 @@ function activateSymbolFilter(){
     }
 }
 
+// Sets the password lenght
 function setPasswordLength(){
     passwordLength = document.getElementById("password-length").value
 }
 
+// Copies the password from each field
 function copyPassword(field){
     if (!(passwordField1.value === "Generate password")){
         if (field === 1) {
